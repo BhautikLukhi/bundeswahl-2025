44bde6bcc312f623597d673c4961d7c07abf9a71
bydata <ansgar.wolsing@gmail.com>
2025-01-27 21:12:23 +0100

    Titel & Annotationen für Chart mit Mehrheiten im Bundesrat

--- R/custom-theme.R
+++ R/custom-theme.R
@@ -5,14 +5,15 @@ theme_custom <- function(base_size = 13, ...) {
   theme_minimal(base_family = "Roboto Condensed", base_size = base_size) +
     theme(
       plot.background = element_rect(color = "white", fill = "white"),
-      plot.margin = margin(t = 4, l = 6, r = 6, b = 4),
+      plot.margin = margin(t = 4, l = 6, r = 6, b = 2),
       plot.title = element_markdown(
         family = "Roboto Slab", face = "bold", lineheight = 1.25),
       plot.title.position = "plot",
       plot.subtitle = element_textbox(
         family = "Roboto Slab", lineheight = 1.25, width = 1),
       plot.caption = element_textbox(
-        family = "Roboto Slab", hjust = 0, lineheight = 1.1, width = 1)
+        family = "Roboto Slab", hjust = 0, lineheight = 1.1, width = 1,
+        margin = margin(t = 4, b = 8))
     )  
 }
 
@@ -29,6 +30,8 @@ color_pal <- c("#1b1b3a", "#216869", "#c17dfa", "#FA8334", "#9EC5AB", "#F2CD5D",
 party_pal <- c(
   "SPD" = "#E3000F",
   "CDU/CSU" = "#000000",
+  "CDU" = "#000000",
+  "CSU" = "#000000",
   "FDP" = "#FFEF00",
   "Grüne" = "#1AA037",
   "AfD" = "#0489DB",
